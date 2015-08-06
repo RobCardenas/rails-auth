e70e0a181ba7c686fed57b4c1d1c80e8086b4bfd
Rob <robbcardenas@gmail.com>
2015-08-05 18:59:56 -0700

    signup working

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:notice] = "You're in!"
+      # redirect_to "/profile"
+      # refactored with route helpers:
+      redirect_to profile_path
+    else
+      flash[:error] = user.errors.full_messages.join(', ')
+      # redirect_to "/signup"
+      # refactored with route helpers:
+      redirect_to signup_path
+    end
+  end
+
+  def show
+    @user = User.find(session[:user_id])
+    render :show
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
+end
